bd12a6e4912540535fa20d8229db447215815393
Dankmod420 <thormodx@gmail.com>
2024-03-28 01:48:28 +0100

    header done

--- education/src/Home_Page/home_page.tsx
+++ education/src/Home_Page/home_page.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import Modal from "react-modal";
+
 function HomePage() {
-  return <div></div>;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  useEffect(() => {
+    setModalIsOpen(true);
+  }, []);
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        style={{
+          content: {
+            width: "50%",
+            height: "90%",
+            margin: "auto",
+            textAlign: "center",
+          },
+        }}
+      >
+        <h2>Create user to compare study programs</h2>
+        <p>content of the modal.</p>
+        <button onClick={closeModal}>No, thanks</button>
+        <button onClick={closeModal}>Create user</button>
+      </Modal>
+    </div>
+  );
 }
 
 export default HomePage;
