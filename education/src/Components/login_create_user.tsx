25cea82b67821d11febe57452124b511fa009d09
MathiasHolmMariager <math214j@hotmail.com>
2024-04-11 14:04:14 +0200

    High school and Bachelor diploma options when creating user + add it to firebase :anguished: :do_not_litter:

--- education/src/Components/login_create_user.tsx
+++ education/src/Components/login_create_user.tsx
@@ -6,6 +6,9 @@ import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 import { auth } from "./firebase";
 import { getDatabase, ref, set } from "firebase/database";
+import Select from "react-select";
+import { highSchoolDiplomaDict } from "./high_school_diploma";
+import { bachelorDiplomaDict } from "./bachelor_diploma";
 
 Modal.setAppElement("#root");
 
@@ -18,7 +21,11 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onRequestClose }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isSigningUp, setIsSigningUp] = useState(false);
-
+  const [highSchoolText, setHighSchoolText] = useState('Hent gymnasiebevis');
+  const [bachelorText, setBachelorText] = useState('Hent bachelorbevis');
+  const [highSchoolDiploma, setHighSchoolDiploma] = useState({});
+  const [bachelorDiploma, setBachelorDiploma] = useState({});
+  
   useEffect(() => {
     if (isOpen) {
       setEmail("");
@@ -35,17 +42,19 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onRequestClose }) => {
   const handleSignup = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     try {
-      await createUserWithEmailAndPassword(auth, email, password).then(
-        (userCredential) => {
-          const user = userCredential.user.uid;
-          const db = getDatabase();
-          set(ref(db, "users/" + user), {
-            email: email,
-          });
-          localStorage.setItem("UID", JSON.stringify(user));
-          handleCloseModal();
-        }
-      );
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user.uid;
+      const db = getDatabase();
+      const userData = {
+        email: email,
+      };
+      set(ref(db, "users/" + user), userData);
+      set(ref(db, "users/" + user+ "/diploma"), {
+        highSchoolDiploma: highSchoolDiploma,
+        bachelorDiploma: bachelorDiploma
+      })
+      localStorage.setItem("UID", JSON.stringify(user));
+      handleCloseModal();
     } catch (error) {
       console.error("Error signing up:", error);
     }
@@ -72,6 +81,32 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onRequestClose }) => {
   const handleKeyPress = (e: React.KeyboardEvent) => {
     if (e.key === "Enter") {
       isSigningUp ? handleSignup(e) : handleLogin(e);
+    }
+  };
+
+  const interests = [
+    { value: "matematik", label: "Matematik" },
+    { value: "fysik", label: "Fysik" },
+    { value: "programmering", label: "Programmering" },
+  ];
+
+  const highSchoolClick = () => {
+    if (highSchoolText === 'Hent gymnasiebevis') {
+      setHighSchoolText('Gemt');
+      setHighSchoolDiploma(highSchoolDiplomaDict);
+    } else {
+      setHighSchoolText('Hent gymnasiebevis');
+      setHighSchoolDiploma({});
+    }
+  };
+
+  const bachelorClick = () => {
+    if (bachelorText === 'Hent bachelorbevis') {
+      setBachelorText('Gemt');
+      setBachelorDiploma(bachelorDiplomaDict);
+    } else {
+      setBachelorText('Hent bachelorbevis');
+      setBachelorDiploma({});
     }
   };
 
@@ -113,6 +148,28 @@ const UserModal: React.FC<UserModalProps> = ({ isOpen, onRequestClose }) => {
                 style={{ width: "250px", height: "30px", padding: "10px" }}
               />
             </div>
+            <div style={{ width: "60%", margin: "auto", marginTop: "20px" }}>
+              <Select
+                options={interests}
+                placeholder="Vælg interesser (valgfri)"
+                isMulti
+                
+              />
+            </div>
+            <button
+              type="button"
+              onClick={highSchoolClick}
+              style={{ margin: "20px", width: "60%" }}
+            >
+              {highSchoolText}
+            </button>
+            <button
+              type="button"
+              onClick={bachelorClick}
+              style={{ margin: "20px", width: "60%" }}
+            >
+              {bachelorText}
+            </button>
             <button
               onClick={handleCloseModal}
               style={{ margin: "20px", width: "120px" }}
