2f029ddda15f2ea411048bbba7a08468bb87c120
N1k50 <nikolaj81561606233@live.dk>
2024-04-11 11:14:14 +0200

    StudyPage update :blue_car: :speak_no_evil: :currency_exchange:
    Updated information on the study page

---
+++ education/src/Study_Program_Page/Dictionaries/IxdBach.tsx
@@ -0,0 +1,86 @@
+const dropdownContent = {
+  "Beskrivelse": 
+  `På uddannelsen lærer du at designe interaktive, 
+  digitale løsninger med fokus på brugeroplevelse, anvendelseskvalitet 
+  og æstetik. Du skal< bl.a. fordybe dig i visuelt design, prototyping, 
+  usability og user experience. Du bliver en nøglespiller i fremtidens produktudvikling.`,
+
+  "Adgangskrav": 
+  `- Bestået adgangsgivende eksamen 
+  <br>- Dansk A 
+  <br>- Engelsk B 
+  <br>- Matematik B`,
+
+  "Adgangskvotient": 
+  `Alle optaget (2023)`,
+
+  "Kandidat muligheder": 
+  `- Interaktionsdesign 
+  <br>- Service Systems Design 
+  <br>- Informationsvidenskab 
+  <br>- Information Studies (Human Centered Informatics) 
+  <br>- Computer Science (IT)`,
+
+  "Semestre": 
+  `<font size="5"><b><u>1 Semester</u></b></font>
+  <br>Projekter:
+  <br><i>- Det mindre projekt: Interaktionsdesign (15 ECTS)</i>
+  <br><i>- Det større projekt: Evaluering af design (15 ECTS)</i>
+  <br>Kurser:
+  <br><i>- Usability og skitsering (5 ECTS)</i>
+  <br><i>- Grundlæggende programmering (5 ECTS)</i>
+  <br><i>- Problembaseret læring (5 ECTS)</i>
+  <br>
+  <br><font size="5"><b><u>2 Semester</u></b></font>
+  <br>Projekter:
+  <br><i>- Design og konstruktion af en webapplikation (15 ECTS)</i>
+  <br>Kurser:
+  <br><i>- Funktionelt og grafisk design (5 ECTS)</i>
+  <br><i>- Anvendt grundlæggende programmering (5 ECTS)</i>
+  <br><i>- Datalogiens matematiske grundlag (5 ECTS)</i>
+  <br>
+  <br><font size="5"><b><u>3 Semester</u></b></font>
+  <br>Projekter:
+  <br><i>- Design med brugere (15 ECTS)</i>
+  <br>Kurser:
+  <br><i>- Brugercentreret design (5 ECTS)</i>
+  <br><i>- Databaseudvikling (5 ECTS)</i>
+  <br><i>- Grundlæggende objektorienteret programmering (5 ECTS)</i>
+  <br>
+  <br><font size="5"><b><u>4 Semester</u></b></font>
+  <br>Projekter (vælg en):
+  <br><i>- Interaktivt fysisk design (15 ECTS)</i>
+  <br><i>- Interaktivt mobilt applikationsdesign (15 ECTS)</i>
+  <br>Kurser:
+  <br><i>- Interaktivt fysisk design (5 ECTS)</i>
+  <br><i>- Kvalitativ metode (5 ECTS)</i>
+  <br><i>- Programmering af mobile applikationer (5 ECTS)</i>
+  <br>
+  <br><font size="5"><b><u>5 Semester</u></b></font>
+  <br>Projekter:
+  <br><i>- Design af interaktion med data (15 ECTS)</i>
+  <br>Kurser:
+  <br><i>- Brugercentreret interaktion med kunstig intelligens(5 ECTS)</i>
+  <br><i>- Kvantitativ metode (5 ECTS)</i>
+  <br><i>- Systemanalyse og design (5 ECTS)</i>
+  <br>
+  <br><font size="5"><b><u>6 Semester</u></b></font>
+  <br>Projekter:
+  <br><i>- Bachelorprojekt (20 ECTS)</i>
+  <br>Kurser:
+  <br><i>- User Experience (5 ECTS)</i>
+  <br><i>- Programmering af IoT applikationer (5 ECTS)</i>`,
+
+  "Frafald": 
+  `- Første år: <i>25%</i>
+  <br>- Andet år: <i>?%</i>
+  <br>- Tredje år: <i>?%</i>`,
+
+  "Tidsforbrug": 
+  `35 timer om ugen
+  <br>- 70% undervisning
+  <br>- 30% forberedelse`,
+
+};
+
+export default dropdownContent;
--- education/src/Study_Program_Page/study_program_page.tsx
+++ education/src/Study_Program_Page/study_program_page.tsx
@@ -3,15 +3,20 @@ import Interaction_Design from "../assets/Interaction_Design.jpg";
 import Star from "../assets/Star.png";
 import StarGold from "../assets/Star_Gold.png";
 import "./study_program_page.css";
+import dropdownContent from "./Dictionaries/IxdBach";
 import { User, getAuth, onAuthStateChanged } from "firebase/auth";
 import { get, getDatabase, ref, remove, set } from "firebase/database";
 import Chatbox from "../Components/ChatBox/chatbox";
+import GoogleMaps from "./Maps/IxdBachMaps";
 
 function StudyProgramPage() {
   const [dropdown1Visible, setDropdown1Visible] = useState(false);
   const [dropdown2Visible, setDropdown2Visible] = useState(false);
   const [dropdown3Visible, setDropdown3Visible] = useState(false);
   const [dropdown4Visible, setDropdown4Visible] = useState(false);
+  const [dropdown5Visible, setDropdown5Visible] = useState(false);
+  const [dropdown6Visible, setDropdown6Visible] = useState(false);
+  const [dropdown7Visible, setDropdown7Visible] = useState(false);
   const [isStarClicked, setIsStarClicked] = useState(false);
   const [uid, setUid] = useState<string | null>(null);
 
@@ -76,6 +81,15 @@ function StudyProgramPage() {
       case 4:
         setDropdown4Visible(!dropdown4Visible);
         break;
+      case 5:
+        setDropdown5Visible(!dropdown5Visible);
+        break;
+      case 6:
+        setDropdown6Visible(!dropdown6Visible);
+        break;
+      case 7:
+        setDropdown7Visible(!dropdown7Visible);
+        break;
       default:
         break;
     }
@@ -117,114 +131,92 @@ function StudyProgramPage() {
       <div className="content">
         <div className="text">
           <p>
-            På uddannelsen lærer du at designe interaktive, digitale løsninger
-            med fokus på brugeroplevelse, anvendelseskvalitet og æstetik. Du
-            skal bl.a. fordybe dig i visuelt design, prototyping, usability og
-            user experience. Du bliver en nøglespiller i fremtidens
-            produktudvikling.
+            {dropdownContent["Beskrivelse"]}
           </p>
         </div>
         <div className="picture">
-          <img src={Interaction_Design} />
+          <img src={Interaction_Design}/>
         </div>
       </div>
       <div className="dropdowns">
+
         <div className="dropdown">
           <button className="dropdown-button" onClick={() => toggleDropdown(1)}>
-            Job muligheder
+            Adgangskrav
           </button>
           {dropdown1Visible && (
             <div className="dropdown-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              pellentesque faucibus molestie. Suspendisse id posuere ante, a
-              porta mauris. Fusce sodales augue nec eros porta lobortis. Integer
-              commodo erat vitae elit viverra, vehicula ornare ligula rhoncus.
-              Aliquam hendrerit metus lectus, eu tincidunt dolor vestibulum id.
-              Cras nec ex lectus. Sed at arcu sed enim facilisis consectetur nec
-              nec turpis. Sed euismod dolor ac velit semper, ac blandit est
-              consequat. Integer tincidunt molestie volutpat. Aliquam eget odio
-              sed metus malesuada bibendum quis at dui. Mauris gravida, ipsum
-              sed interdum varius, quam sem scelerisque diam, ut ornare neque
-              mauris ut dui. Maecenas mollis mi magna, at volutpat libero
-              viverra a. Phasellus sit amet risus rutrum, interdum tellus sit
-              amet, mattis purus. Nam vehicula nisl velit, ac pellentesque ipsum
-              lobortis et. Mauris scelerisque lectus sit amet nibh congue
-              tempus.
+              <div dangerouslySetInnerHTML={{ __html: dropdownContent["Adgangskrav"] }}/>
             </div>
           )}
         </div>
+
         <div className="dropdown">
           <button className="dropdown-button" onClick={() => toggleDropdown(2)}>
-            Videre uddanelse
+            Adgangskvotient
           </button>
           {dropdown2Visible && (
             <div className="dropdown-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              pellentesque faucibus molestie. Suspendisse id posuere ante, a
-              porta mauris. Fusce sodales augue nec eros porta lobortis. Integer
-              commodo erat vitae elit viverra, vehicula ornare ligula rhoncus.
-              Aliquam hendrerit metus lectus, eu tincidunt dolor vestibulum id.
-              Cras nec ex lectus. Sed at arcu sed enim facilisis consectetur nec
-              nec turpis. Sed euismod dolor ac velit semper, ac blandit est
-              consequat. Integer tincidunt molestie volutpat. Aliquam eget odio
-              sed metus malesuada bibendum quis at dui. Mauris gravida, ipsum
-              sed interdum varius, quam sem scelerisque diam, ut ornare neque
-              mauris ut dui. Maecenas mollis mi magna, at volutpat libero
-              viverra a. Phasellus sit amet risus rutrum, interdum tellus sit
-              amet, mattis purus. Nam vehicula nisl velit, ac pellentesque ipsum
-              lobortis et. Mauris scelerisque lectus sit amet nibh congue
-              tempus.
+              <div dangerouslySetInnerHTML={{ __html: dropdownContent["Adgangskvotient"] }}/>
             </div>
           )}
         </div>
+
         <div className="dropdown">
           <button className="dropdown-button" onClick={() => toggleDropdown(3)}>
-            De forskellige semestre
+            Kandidat muligheder
           </button>
           {dropdown3Visible && (
             <div className="dropdown-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              pellentesque faucibus molestie. Suspendisse id posuere ante, a
-              porta mauris. Fusce sodales augue nec eros porta lobortis. Integer
-              commodo erat vitae elit viverra, vehicula ornare ligula rhoncus.
-              Aliquam hendrerit metus lectus, eu tincidunt dolor vestibulum id.
-              Cras nec ex lectus. Sed at arcu sed enim facilisis consectetur nec
-              nec turpis. Sed euismod dolor ac velit semper, ac blandit est
-              consequat. Integer tincidunt molestie volutpat. Aliquam eget odio
-              sed metus malesuada bibendum quis at dui. Mauris gravida, ipsum
-              sed interdum varius, quam sem scelerisque diam, ut ornare neque
-              mauris ut dui. Maecenas mollis mi magna, at volutpat libero
-              viverra a. Phasellus sit amet risus rutrum, interdum tellus sit
-              amet, mattis purus. Nam vehicula nisl velit, ac pellentesque ipsum
-              lobortis et. Mauris scelerisque lectus sit amet nibh congue
-              tempus.
+              <div dangerouslySetInnerHTML={{ __html: dropdownContent["Kandidat muligheder"] }}/>
             </div>
           )}
         </div>
+
         <div className="dropdown">
           <button className="dropdown-button" onClick={() => toggleDropdown(4)}>
-            Statistikker
+            Lokation
           </button>
           {dropdown4Visible && (
             <div className="dropdown-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              pellentesque faucibus molestie. Suspendisse id posuere ante, a
-              porta mauris. Fusce sodales augue nec eros porta lobortis. Integer
-              commodo erat vitae elit viverra, vehicula ornare ligula rhoncus.
-              Aliquam hendrerit metus lectus, eu tincidunt dolor vestibulum id.
-              Cras nec ex lectus. Sed at arcu sed enim facilisis consectetur nec
-              nec turpis. Sed euismod dolor ac velit semper, ac blandit est
-              consequat. Integer tincidunt molestie volutpat. Aliquam eget odio
-              sed metus malesuada bibendum quis at dui. Mauris gravida, ipsum
-              sed interdum varius, quam sem scelerisque diam, ut ornare neque
-              mauris ut dui. Maecenas mollis mi magna, at volutpat libero
-              viverra a. Phasellus sit amet risus rutrum, interdum tellus sit
-              amet, mattis purus. Nam vehicula nisl velit, ac pellentesque ipsum
-              lobortis et. Mauris scelerisque lectus sit amet nibh congue
-              tempus.
+              <GoogleMaps/>
             </div>
           )}
         </div>
+
+        <div className="dropdown">
+          <button className="dropdown-button" onClick={() => toggleDropdown(5)}>
+            Semestre
+          </button>
+          {dropdown5Visible && (
+            <div className="dropdown-content">
+              <div dangerouslySetInnerHTML={{ __html: dropdownContent["Semestre"] }}/>
+            </div>
+          )}
+        </div>
+
+        <div className="dropdown">
+          <button className="dropdown-button" onClick={() => toggleDropdown(6)}>
+            Frafald
+          </button>
+          {dropdown6Visible && (
+            <div className="dropdown-content">
+              <div dangerouslySetInnerHTML={{ __html: dropdownContent["Frafald"] }}/>
+            </div>
+          )}
+        </div>
+
+        <div className="dropdown">
+          <button className="dropdown-button" onClick={() => toggleDropdown(7)}>
+            Tidsforbrug
+          </button>
+          {dropdown7Visible && (
+            <div className="dropdown-content">
+              <div dangerouslySetInnerHTML={{ __html: dropdownContent["Tidsforbrug"] }}/>
+            </div>
+          )}
+        </div>
+        
       </div>
       <Chatbox />
     </div>
